7d03fbc04667813acf44b09c70aef95661a0c202
JasonLin <JasonLin1230@163.com>
2018-02-16 19:07:08 +0800

    更换API

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,12 +3,11 @@ const app = getApp()
 var latitude;
 var longitude;
 var tip_arr = ['建议着轻棉织物制作的短衣、短裙、短裤等服装','建议着棉麻面料的衬衫、薄长裙、薄T恤等服装','建议穿薄外套或牛仔裤等服装。','建议着厚外套加毛衣等服装。','建议着棉衣加羊毛衫等冬季服装。','建议着厚羽绒服等隆冬服装。'];
-var Url = "https://free-api.heweather.com/s6/weather/now"
-var ForeUrl ="https://free-api.heweather.com/s6/weather/forecast"
+var Url = "https://free-api.heweather.com/s6/weather"
 var Key = "c8216c197e6049f3a4ff432524b06499"
 Page({
   data: {
-    motto: 'Copyright © 2018.JasonLin',
+    motto: 'Copyright © 2018.Jason Lin',
     userChoosedLocation:'手动选择位置',
     hasUserLocation: false,
   },
@@ -18,44 +17,7 @@ Page({
     var address=address
     var latitude=latitude
     var longitude=longitude
-    var all_ready=false
-    var foreurl = ForeUrl + "?location=" + longitude + ',' + latitude + "&key=" + Key
     var url = Url + "?location=" + longitude + ',' + latitude + "&key=" + Key
-    wx.request({
-      url: foreurl,
-      header: {
-        'content-type': 'application/json'
-      },
-      success: function (res) {
-        if (res.data.HeWeather6[0].status == 'ok') {
-          var forecast = res.data.HeWeather6[0].daily_forecast
-          var cond_code_tomo=forecast[1].cond_code_d
-          var cond_code_after = forecast[2].cond_code_d
-          var cond_txt_tomo = forecast[1].cond_txt_d
-          var cond_txt_after = forecast[2].cond_txt_d
-          var tmp_tomo_max = forecast[1].tmp_max
-          var tmp_after_max = forecast[2].tmp_max
-          var tmp_tomo_min = forecast[1].tmp_min
-          var tmp_after_min = forecast[2].tmp_min
-          var cond_icon_tomo = '../../images/' + cond_code_tomo + '.png'
-          var cond_icon_after = '../../images/' + cond_code_after + '.png'
-          var tmp_tomo = tmp_tomo_min + '~' + tmp_tomo_max + '℃'
-          var tmp_after = tmp_after_min + '~' + tmp_after_max + '℃'
-          that.setData({
-            cond_icon_tomo: cond_icon_tomo,
-            cond_icon_after: cond_icon_after,
-            cond_txt_tomo: cond_txt_tomo,
-            cond_txt_after: cond_txt_after,
-            tmp_tomo: tmp_tomo,
-            tmp_after: tmp_after
-          })
-        }
-        if(all_ready==true){
-          wx.hideLoading()
-        }
-        all_ready=true
-      }
-    })
     wx.request({
       url: url,
       header: {
@@ -63,6 +25,7 @@ Page({
       },
       success: function (res) {
         if (res.data.HeWeather6[0].status == 'ok') {
+          console.log(res.data.HeWeather6[0])
           var basic = res.data.HeWeather6[0].basic
           var now = res.data.HeWeather6[0].now
           var update = res.data.HeWeather6[0].update
@@ -90,13 +53,32 @@ Page({
             tip = tip_arr[5]
           }
           tmp = tmp + '℃'
+          var forecast = res.data.HeWeather6[0].daily_forecast
+          var cond_code_tomo=forecast[1].cond_code_d
+          var cond_code_after = forecast[2].cond_code_d
+          var cond_txt_tomo = forecast[1].cond_txt_d
+          var cond_txt_after = forecast[2].cond_txt_d
+          var tmp_tomo_max = forecast[1].tmp_max
+          var tmp_after_max = forecast[2].tmp_max
+          var tmp_tomo_min = forecast[1].tmp_min
+          var tmp_after_min = forecast[2].tmp_min
+          var cond_icon_tomo = '../../images/' + cond_code_tomo + '.png'
+          var cond_icon_after = '../../images/' + cond_code_after + '.png'
+          var tmp_tomo = tmp_tomo_min + '~' + tmp_tomo_max + '℃'
+          var tmp_after = tmp_after_min + '~' + tmp_after_max + '℃'
           that.setData({
             update_loc: last_time_loc,
             cond_icon: cond_icon,
             cond_txt: cond_txt,
             tmp: tmp,
             hum: hum,
-            tip: tip
+            tip: tip,
+            cond_icon_tomo: cond_icon_tomo,
+            cond_icon_after: cond_icon_after,
+            cond_txt_tomo: cond_txt_tomo,
+            cond_txt_after: cond_txt_after,
+            tmp_tomo: tmp_tomo,
+            tmp_after: tmp_after
           })
           if (address===null) {
             var autoLocation=city
@@ -107,14 +89,12 @@ Page({
               autoLocation = admin_area+'-'+autoLocation
             }
             that.setData({
-              userChoosedLocation: autoLocation
+              userChoosedLocation: autoLocation,
+              hasUserLocation: true
             })
           }
         }
-        if (all_ready == true) {
-          wx.hideLoading()
-        }
-        all_ready = true
+        wx.hideLoading()
       }
     })
   },
@@ -146,7 +126,6 @@ Page({
       success: res => {
         var latitude = res.latitude
         var longitude = res.longitude
-        var name = res.name
         var address = res.address
         if (address.length > 18) {
           address = address.substring(0, 18)
