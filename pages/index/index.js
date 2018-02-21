9b2c728433c8f5bcc898b3cfad45cc3cb104e367
JasonLin <JasonLin1230@163.com>
2018-02-21 18:19:42 +0800

    UI

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,13 +1,13 @@
 //获取应用实例
 const app = getApp()
+// const echarts = require("../../utils/echarts.simple.min.js");
 var latitude;
 var longitude;
-var tip_arr = ['建议着轻棉织物制作的短衣、短裙、短裤等服装','建议着棉麻面料的衬衫、薄长裙、薄T恤等服装','建议穿薄外套或牛仔裤等服装。','建议着厚外套加毛衣等服装。','建议着棉衣加羊毛衫等冬季服装。','建议着厚羽绒服等隆冬服装。'];
 var Url = "https://free-api.heweather.com/s6/weather"
 var Key = "c8216c197e6049f3a4ff432524b06499"
 Page({
   data: {
-    motto: 'Copyright © 2018.Jason Lin',
+    motto: 'Copyright © 2018. Jason Lin',
     userChoosedLocation:'手动选择位置',
     hasUserLocation: false,
   },
@@ -25,60 +25,80 @@ Page({
       },
       success: function (res) {
         if (res.data.HeWeather6[0].status == 'ok') {
-          console.log(res.data.HeWeather6[0])
           var basic = res.data.HeWeather6[0].basic
           var now = res.data.HeWeather6[0].now
           var update = res.data.HeWeather6[0].update
+          var lifestyle = res.data.HeWeather6[0].lifestyle
           var admin_area = basic.admin_area
           var parent_city = basic.parent_city
           var city = basic.location
+          // now
           var cond_code = now.cond_code
           var cond_txt = now.cond_txt
-          var tmp = now.tmp
+          var tmp = now.tmp + '℃'
           var hum = now.hum + '%'
+          var wind_dir = now.wind_dir
+          var wind_sc = now.wind_sc
+          if(wind_sc.indexOf('风')===-1){
+            wind_sc = wind_sc + '级'
+          }
+          var fl = now.fl + '℃'
+          var vis = now.vis + 'km'
+          var pres = now.pres + 'mb'
+
           var last_time_loc = update.loc
           var cond_icon = '../../images/' + cond_code + '.png'
-          var tip
-          if (tmp > 28) {
-            tip = tip_arr[0]
-          } else if (tmp > 22) {
-            tip = tip_arr[1]
-          } else if (tmp > 18) {
-            tip = tip_arr[2]
-          } else if (tmp > 10) {
-            tip = tip_arr[3]
-          } else if (tmp > 0) {
-            tip = tip_arr[4]
-          } else {
-            tip = tip_arr[5]
-          }
-          tmp = tmp + '℃'
           var forecast = res.data.HeWeather6[0].daily_forecast
-          var cond_code_tomo=forecast[1].cond_code_d
+          // forecast
+          var cond_code_today = forecast[0].cond_code_d
+          var cond_code_tomo = forecast[1].cond_code_d
           var cond_code_after = forecast[2].cond_code_d
+          var cond_txt_today = forecast[0].cond_txt_d
           var cond_txt_tomo = forecast[1].cond_txt_d
           var cond_txt_after = forecast[2].cond_txt_d
+          var tmp_today_max = forecast[0].tmp_max
           var tmp_tomo_max = forecast[1].tmp_max
           var tmp_after_max = forecast[2].tmp_max
+          var tmp_today_min = forecast[0].tmp_min
           var tmp_tomo_min = forecast[1].tmp_min
           var tmp_after_min = forecast[2].tmp_min
+          var cond_icon_today = '../../images/' + cond_code_today + '.png'
           var cond_icon_tomo = '../../images/' + cond_code_tomo + '.png'
           var cond_icon_after = '../../images/' + cond_code_after + '.png'
+          var tmp_today = tmp_today_min + '~' + tmp_today_max + '℃'
           var tmp_tomo = tmp_tomo_min + '~' + tmp_tomo_max + '℃'
           var tmp_after = tmp_after_min + '~' + tmp_after_max + '℃'
+          // lifestyle
+          var drsg = lifestyle[1].brf
+          var flu = lifestyle[2].brf
+          var sport = lifestyle[3].brf
+          var air = lifestyle[7].brf
           that.setData({
             update_loc: last_time_loc,
             cond_icon: cond_icon,
             cond_txt: cond_txt,
             tmp: tmp,
             hum: hum,
-            tip: tip,
+            wind_dir: wind_dir,
+            wind_sc: wind_sc,
+            fl: fl,
+            vis: vis,
+            pres: pres,
+
+            cond_icon_today: cond_icon_today,
             cond_icon_tomo: cond_icon_tomo,
             cond_icon_after: cond_icon_after,
+            cond_txt_today: cond_txt_today,
             cond_txt_tomo: cond_txt_tomo,
             cond_txt_after: cond_txt_after,
+            tmp_today: tmp_today,
             tmp_tomo: tmp_tomo,
-            tmp_after: tmp_after
+            tmp_after: tmp_after,
+
+            drsg: drsg,
+            flu: flu,
+            sport: sport,
+            air: air
           })
           if (address===null) {
             var autoLocation=city
@@ -116,6 +136,13 @@ Page({
           longitude = res.longitude;
           var address=null
           this.getWeatherData(that,address,latitude,longitude)
+        },
+        fail:function(res){
+          wx.showToast({
+            title: '请授权位置',
+            icon: 'loading',
+            duration: 2000
+          })
         }
       })
     }
@@ -137,6 +164,13 @@ Page({
           hasUserLocation: true
         })
       }
+    })
+  },
+  week_forecast: function(){
+    wx.showToast({
+      title: '本功能将在小程序支持echarts后上线',
+      icon: 'none',
+      duration: 2000
     })
   }
 })
