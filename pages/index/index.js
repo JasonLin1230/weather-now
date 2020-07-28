80931d12fd23b7a339ac2fe429c9ade398ac6a1e
JasonLin <JasonLin1230@163.com>
2020-07-28 22:24:13 +0800

    lifestyle v7

--- pages/index/index.js
+++ pages/index/index.js
@@ -29,9 +29,9 @@ Page({
     var address = address
     var latitude = latitude
     var longitude = longitude
-    var url = baseUrl + "now?location=" + longitude + ',' + latitude + "&key=" + Key
+    // 获取实况天气
     wx.request({
-      url: url,
+      url: baseUrl + "now?location=" + longitude + ',' + latitude + "&key=" + Key,
       header: {
         'content-type': 'application/json'
       },
@@ -42,66 +42,16 @@ Page({
           var now = data.now
           // 更新时间
           var last_time_loc = data.updateTime
-          let timeZone = last_time_loc.indexOf("+")
-          last_time_loc = last_time_loc.replace("T"," ").slice(0,timeZone);
+          last_time_loc = last_time_loc.replace("T"," ").slice(0,16);
           // 风力级别
           if (now.windScale.indexOf('风') === -1) {
             now.windScale = now.windScale + '级'
           }
-          // var basic = res.data.HeWeather6[0].basic
-          // var update = res.data.HeWeather6[0].update
-          // var lifestyle = res.data.HeWeather6[0].lifestyle
-          // var hourly = res.data.HeWeather6[0].hourly
-          // var forecast = res.data.HeWeather6[0].daily_forecast
-          // var admin_area = basic.admin_area
-          // var parent_city = basic.parent_city
-          // var city = basic.location
-          // now
-          // var now_format = []
-          // now_format.push(now)
-          // if (now_format[0].wind_sc.indexOf('风') === -1) {
-          //   now_format[0].wind_sc = now_format[0].wind_sc + '级'
-          // }
-          // // forecast
-          // forecast[0].day = "今天"
-          // forecast[1].day = "明天"
-          // forecast[2].day = "后天"
-          // // hourly
-          // hourly = JSON.parse(JSON.stringify(hourly))
-          // var hourly_format = [];
-          // hourly.forEach(function (item) {
-          //   hourly_format.push({
-          //     'time': item.time.substring(11),
-          //     'cond_code': item.cond_code,
-          //     'tmp': item.tmp,
-          //     'wind_dir': item.wind_dir,
-          //     'wind_sc': (item.wind_sc.indexOf('风') === -1 ? item.wind_sc + '级' : item.wind_sc)
-          //   })
-          // })
           that.setData({
             update_loc: last_time_loc,
             now: now,
-            // forecast: forecast,
-            // hourly: hourly_format,
-            // lifestyle: lifestyle,
             motto: 'Copyright © 2017-2020. Jason Lin'
           })
-          // global
-          // app.globalData.daily_forecast = forecast
-          // app.globalData.lifestyle = lifestyle
-          // if (address === null) {
-          //   var autoLocation = city
-          //   if (parent_city != city) {
-          //     autoLocation = parent_city + '-' + autoLocation
-          //   }
-          //   if (admin_area != parent_city && admin_area != city) {
-          //     autoLocation = admin_area + '-' + autoLocation
-          //   }
-          //   that.setData({
-          //     userChoosedLocation: autoLocation,
-          //     hasUserLocation: true
-          //   })
-          // }
         }else{
           wx.showToast({
             title: '数据请求失败',
@@ -112,9 +62,6 @@ Page({
             wx.hideToast()
           }, 2000)
         }
-        wx.hideLoading()
-        wx.hideNavigationBarLoading()
-        wx.stopPullDownRefresh()
       },
       fail: function(){
         wx.showToast({
@@ -127,6 +74,84 @@ Page({
         }, 2000)
       }
     }),
+    // 获取七天预报
+    wx.request({
+      url: baseUrl+"7d?location=" + longitude + ',' + latitude + "&key=" + Key+"&type=0",
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        console.log(res.data)
+        if(res.data.code == '200'){
+          let forecast = res.data.daily
+          forecast[0].day = "今天"
+          forecast[1].day = "明天"
+          forecast[2].day = "后天"
+          that.setData({
+            forecast: forecast
+          })
+        }else{
+          wx.showToast({
+            title: '数据请求失败',
+            icon: 'loading',
+            duration: 2000
+          })
+          setTimeout(function () {
+            wx.hideToast()
+          }, 2000)
+        }
+      },
+      fail: function(){
+        wx.showToast({
+          title: '异常，请重启',
+          icon: 'loading',
+          duration: 2000
+        })
+        setTimeout(function () {
+          wx.hideToast()
+        }, 2000)
+      }
+    }),
+    // 获取24小时预报
+    wx.request({
+      url: baseUrl+"24h?location=" + longitude + ',' + latitude + "&key=" + Key+"&type=0",
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        console.log(res.data)
+        if(res.data.code == '200'){
+          let hourly = res.data.hourly
+          hourly.forEach(item => {
+            item.fxTime = item.fxTime.slice(11,16)
+            item.windScale = (item.windScale.indexOf('风') === -1 ? item.windScale + '级' : item.windScale)
+          })
+          that.setData({
+            hourly: hourly
+          })
+        }else{
+          wx.showToast({
+            title: '数据请求失败',
+            icon: 'loading',
+            duration: 2000
+          })
+          setTimeout(function () {
+            wx.hideToast()
+          }, 2000)
+        }
+      },
+      fail: function(){
+        wx.showToast({
+          title: '异常，请重启',
+          icon: 'loading',
+          duration: 2000
+        })
+        setTimeout(function () {
+          wx.hideToast()
+        }, 2000)
+      }
+    }),
+    // 获取当天生活指数
     wx.request({
       url: lifeUrl+"1d?location=" + longitude + ',' + latitude + "&key=" + Key+"&type=0",
       header: {
@@ -139,6 +164,7 @@ Page({
           that.setData({
             lifestyle: lifestyle
           })
+          app.globalData.lifestyle = lifestyle
         }else{
           wx.showToast({
             title: '数据请求失败',
@@ -161,6 +187,9 @@ Page({
         }, 2000)
       }
     })
+    wx.hideLoading()
+    wx.hideNavigationBarLoading()
+    wx.stopPullDownRefresh()
   },
   //事件处理函数
   onLoad: function () {
