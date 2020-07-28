3f2b22d5decc23fe15039fa7cde9de560ae601b9
JasonLin <JasonLin1230@163.com>
2020-07-28 21:21:25 +0800

    背景色更换、api_v7 now

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,8 @@
 //获取应用实例
 const app = getApp()
 var latitude, longitude, address;
-var Url = "https://free-api.heweather.com/s6/weather"
+var baseUrl = "https://devapi.heweather.net/v7/weather/"
+var lifeUrl = "https://devapi.heweather.net/v7/indices/"
 var Key = "c8216c197e6049f3a4ff432524b06499"
 Page({
   data: {
@@ -28,70 +29,79 @@ Page({
     var address = address
     var latitude = latitude
     var longitude = longitude
-    var url = Url + "?location=" + longitude + ',' + latitude + "&key=" + Key
+    var url = baseUrl + "now?location=" + longitude + ',' + latitude + "&key=" + Key
     wx.request({
       url: url,
       header: {
         'content-type': 'application/json'
       },
       success: function (res) {
-        if (res.data.HeWeather6[0].status == 'ok') {
-          var basic = res.data.HeWeather6[0].basic
-          var now = res.data.HeWeather6[0].now
-          var update = res.data.HeWeather6[0].update
-          var lifestyle = res.data.HeWeather6[0].lifestyle
-          var hourly = res.data.HeWeather6[0].hourly
-          var forecast = res.data.HeWeather6[0].daily_forecast
-          var admin_area = basic.admin_area
-          var parent_city = basic.parent_city
-          var city = basic.location
-          var last_time_loc = update.loc
-          // now
-          var now_format = []
-          now_format.push(now)
-          if (now_format[0].wind_sc.indexOf('风') === -1) {
-            now_format[0].wind_sc = now_format[0].wind_sc + '级'
+        console.log(res.data);
+        if (res.data.code == '200') {
+          var data = res.data
+          var now = data.now
+          // 更新时间
+          var last_time_loc = data.updateTime
+          let timeZone = last_time_loc.indexOf("+")
+          last_time_loc = last_time_loc.replace("T"," ").slice(0,timeZone);
+          // 风力级别
+          if (now.windScale.indexOf('风') === -1) {
+            now.windScale = now.windScale + '级'
           }
-          // forecast
-          forecast[0].day = "今天"
-          forecast[1].day = "明天"
-          forecast[2].day = "后天"
-          // hourly
-          hourly = JSON.parse(JSON.stringify(hourly))
-          var hourly_format = [];
-          hourly.forEach(function (item) {
-            hourly_format.push({
-              'time': item.time.substring(11),
-              'cond_code': item.cond_code,
-              'tmp': item.tmp,
-              'wind_dir': item.wind_dir,
-              'wind_sc': (item.wind_sc.indexOf('风') === -1 ? item.wind_sc + '级' : item.wind_sc)
-            })
-          })
+          // var basic = res.data.HeWeather6[0].basic
+          // var update = res.data.HeWeather6[0].update
+          // var lifestyle = res.data.HeWeather6[0].lifestyle
+          // var hourly = res.data.HeWeather6[0].hourly
+          // var forecast = res.data.HeWeather6[0].daily_forecast
+          // var admin_area = basic.admin_area
+          // var parent_city = basic.parent_city
+          // var city = basic.location
+          // now
+          // var now_format = []
+          // now_format.push(now)
+          // if (now_format[0].wind_sc.indexOf('风') === -1) {
+          //   now_format[0].wind_sc = now_format[0].wind_sc + '级'
+          // }
+          // // forecast
+          // forecast[0].day = "今天"
+          // forecast[1].day = "明天"
+          // forecast[2].day = "后天"
+          // // hourly
+          // hourly = JSON.parse(JSON.stringify(hourly))
+          // var hourly_format = [];
+          // hourly.forEach(function (item) {
+          //   hourly_format.push({
+          //     'time': item.time.substring(11),
+          //     'cond_code': item.cond_code,
+          //     'tmp': item.tmp,
+          //     'wind_dir': item.wind_dir,
+          //     'wind_sc': (item.wind_sc.indexOf('风') === -1 ? item.wind_sc + '级' : item.wind_sc)
+          //   })
+          // })
           that.setData({
             update_loc: last_time_loc,
-            now: now_format,
-            forecast: forecast,
-            hourly: hourly_format,
-            lifestyle: lifestyle,
-            motto: 'Copyright © 2017-2020. Jason Lin',
+            now: now,
+            // forecast: forecast,
+            // hourly: hourly_format,
+            // lifestyle: lifestyle,
+            motto: 'Copyright © 2017-2020. Jason Lin'
           })
           // global
-          app.globalData.daily_forecast = forecast
-          app.globalData.lifestyle = lifestyle
-          if (address === null) {
-            var autoLocation = city
-            if (parent_city != city) {
-              autoLocation = parent_city + '-' + autoLocation
-            }
-            if (admin_area != parent_city && admin_area != city) {
-              autoLocation = admin_area + '-' + autoLocation
-            }
-            that.setData({
-              userChoosedLocation: autoLocation,
-              hasUserLocation: true
-            })
-          }
+          // app.globalData.daily_forecast = forecast
+          // app.globalData.lifestyle = lifestyle
+          // if (address === null) {
+          //   var autoLocation = city
+          //   if (parent_city != city) {
+          //     autoLocation = parent_city + '-' + autoLocation
+          //   }
+          //   if (admin_area != parent_city && admin_area != city) {
+          //     autoLocation = admin_area + '-' + autoLocation
+          //   }
+          //   that.setData({
+          //     userChoosedLocation: autoLocation,
+          //     hasUserLocation: true
+          //   })
+          // }
         }else{
           wx.showToast({
             title: '数据请求失败',
@@ -105,6 +115,40 @@ Page({
         wx.hideLoading()
         wx.hideNavigationBarLoading()
         wx.stopPullDownRefresh()
+      },
+      fail: function(){
+        wx.showToast({
+          title: '异常，请重启',
+          icon: 'loading',
+          duration: 2000
+        })
+        setTimeout(function () {
+          wx.hideToast()
+        }, 2000)
+      }
+    }),
+    wx.request({
+      url: lifeUrl+"1d?location=" + longitude + ',' + latitude + "&key=" + Key+"&type=0",
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        console.log(res.data)
+        if(res.data.code == '200'){
+          let lifestyle = res.data.daily
+          that.setData({
+            lifestyle: lifestyle
+          })
+        }else{
+          wx.showToast({
+            title: '数据请求失败',
+            icon: 'loading',
+            duration: 2000
+          })
+          setTimeout(function () {
+            wx.hideToast()
+          }, 2000)
+        }
       },
       fail: function(){
         wx.showToast({
