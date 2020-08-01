336248ad8a7d2a301c09f36cc05462fe1545df25
JasonLin <JasonLin1230@163.com>
2020-08-01 14:15:01 +0800

    canvas 2d

--- pages/line/line.js
+++ pages/line/line.js
@@ -1,6 +1,4 @@
-import * as echarts from '../../ec-canvas/echarts.min';
-
-let chart = null;
+import * as echarts from '../../ec-canvas/echarts';
 
 const app = getApp();
 
@@ -8,7 +6,13 @@ let dates = [];
 let tempMin = [];
 let tempMax = [];
 
-function setOption(chart) {
+function initChart(canvas,width,height,dpr) {
+  const chart = echarts.init(canvas, null, {
+    width: width,
+    height: height,
+    devicePixelRatio: dpr // new
+  });
+  canvas.setChart(chart);
 
   const option = {
     backgroundColor: "#fff",
@@ -57,6 +61,7 @@ function setOption(chart) {
     ]
   };
   chart.setOption(option);
+  return chart;
 }
 
 Page({
@@ -92,42 +97,41 @@ Page({
   },
   onReady: function () {
     // 获取组件
-    this.ecComponent = this.selectComponent('#mychart-dom-line');
-    this.ecComponent.init((canvas, width, height) => {
-      // 获取组件的 canvas、width、height 后的回调函数
-      // 在这里初始化图表
-      const chart = echarts.init(canvas, null, {
-        width: width,
-        height: height
-      });
-      setOption(chart);
+    // this.ecComponent = this.selectComponent('#mychart-dom-line');
+    // this.ecComponent.init((canvas, width, height) => {
+    //   // 获取组件的 canvas、width、height 后的回调函数
+    //   // 在这里初始化图表
+    //   const chart = echarts.init(canvas, null, {
+    //     width: width,
+    //     height: height
+    //   });
+    //   setOption(chart);
 
-      // 将图表实例绑定到 this 上，可以在其他成员函数（如 dispose）中访问
-      this.chart = chart;
+    //   // 将图表实例绑定到 this 上，可以在其他成员函数（如 dispose）中访问
+    //   this.chart = chart;
 
-      this.setData({
-        isLoaded: true,
-        isDisposed: false
-      });
-    });
+    //   this.setData({
+    //     isLoaded: true,
+    //     isDisposed: false
+    //   });
+    // });
   },
 
   data: {
     ec: {
-      // 将 lazyLoad 设为 true 后，需要手动初始化图表
-      lazyLoad: true
+      onInit: initChart,
     },
-    isLoaded: false,
-    isDisposed: false
+    // isLoaded: false,
+    // isDisposed: false
   },
 
-  dispose: function () {
-    if (this.chart) {
-      this.chart.dispose();
-    }
+  // dispose: function () {
+  //   if (this.chart) {
+  //     this.chart.dispose();
+  //   }
 
-    this.setData({
-      isDisposed: true
-    });
-  }
+  //   this.setData({
+  //     isDisposed: true
+  //   });
+  // }
 });
